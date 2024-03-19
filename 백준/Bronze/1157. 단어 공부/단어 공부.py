704ca5d581b771c1d398aca2ef28545abb189b2b
Kim TaeWoo <97069814+kinemurayasuo@users.noreply.github.com>
2024-03-19 09:33:57 +0900

    [Bronze I] Title: 단어 공부, Time: 236 ms, Memory: 33076 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,16 @@
+word = input().upper()  # 입력된 단어를 모두 대문자로 변환합니다.
+count = [0] * 26  # 알파벳의 등장 횟수를 저장하기 위한 리스트입니다.
+
+for char in word:
+    if char.isalpha():  # 알파벳인 경우에만 처리합니다.
+        count[ord(char) - 65] += 1  # 알파벳의 등장 횟수를 증가시킵니다.
+
+max_count = max(count)  # 가장 많이 등장한 알파벳의 횟수를 구합니다.
+max_index = count.index(max_count)  # 가장 많이 등장한 알파벳의 인덱스를 구합니다.
+
+# 가장 많이 등장한 알파벳의 횟수가 1보다 크면서, 동일한 횟수를 가진 알파벳이 있는 경우 "?"를 출력합니다.
+if count.count(max_count) > 1:
+    print("?")
+else:
+    # 가장 많이 등장한 알파벳의 인덱스를 대문자로 변환하여 출력합니다.
+    print(chr(max_index + 65))
