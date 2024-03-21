f65308140c05a0677a46b847ec279e5c32c20aa6
Kim TaeWoo <97069814+kinemurayasuo@users.noreply.github.com>
2024-03-21 13:29:53 +0900

    [Silver III] Title: Java vs C++, Time: 128 ms, Memory: 34848 KB -BaekjoonHub

---
+++ 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.py
@@ -0,0 +1,54 @@
+from string import ascii_uppercase
+
+def Java_To_C(v):
+    tmp = v[0]
+    if tmp.isupper():
+        return 'Error!'
+    for i in range(1, len(v)):
+        if v[i].isupper():
+            tmp += '_'
+            tmp += v[i].lower()
+        else:
+            tmp += v[i]
+
+    return tmp
+
+def C_to_Java():
+    for i in range(1, len(list(a))):
+        if a[i-1] == a[i] == '_':
+            return 'Error!'
+    if a[0] == '_' or a[-1] == '_':
+        return 'Error!'
+    a_list = a.split('_')
+
+    for i in a_list:
+        if not i.isalnum():
+            return 'Error!'
+        if i.lower() != i:
+            return "Error!"
+    for i in range(len(a_list)):
+        if a_list[i][0].lower() != a_list[i][0]:
+            return 'Error!'
+    for i in range(1, len(a_list)):
+        a_list[i] = a_list[i].capitalize()
+    return ''.join(a_list)
+
+answer_list = []
+java_result_list = []
+
+a = input()
+b = list(ascii_uppercase)
+if '_' in a:
+    if len(answer_list) == 0:
+        answer_list.append(C_to_Java())
+else:
+    if a.lower() == a:
+        print(a)
+    else:
+        for i in b:
+            if i in a:
+                if len(answer_list) == 0:
+                    answer_list.append(Java_To_C(a))
+
+if len(answer_list) > 0:
+    print(''.join(answer_list))
