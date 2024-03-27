47c07da1bc83c3a862a0fac7ace0dbbbe848c68a
Kim TaeWoo <97069814+kinemurayasuo@users.noreply.github.com>
2024-03-27 21:53:05 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,21 +1,21 @@
-def is_vps(s):
+def parenthesis(x):
     stack = []
-    bracket_pairs = {')': '('}  # 닫는 괄호와 대응하는 여는 괄호 매핑
-
-    for char in s:
-        if char in bracket_pairs.values():  # 여는 괄호라면 스택에 추가
-            stack.append(char)
-        elif char in bracket_pairs.keys():  # 닫는 괄호라면
-            if not stack or bracket_pairs[char] != stack.pop():  # 스택이 비어있거나 대응하는 여는 괄호가 아니라면
+    for i in x:
+        if i == '(':
+            stack.append(i)
+        elif i == ')':
+            if not stack:
                 return "NO"
-    
-    if not stack:  # 스택이 비어있다면 올바른 괄호 문자열
+            else:
+                stack.pop()
+    if not stack:
         return "YES"
     else:
         return "NO"
+    
+a = int(input())
+for _ in range(a):
+    x = input()
+    print(parenthesis(x))
 
-# 입력 받기
-T = int(input())  # 테스트 케이스의 수
-for _ in range(T):
-    parentheses = input()  # 괄호 문자열
-    print(is_vps(parentheses))
+    
